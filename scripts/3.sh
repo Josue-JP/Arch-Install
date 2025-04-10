731407018d866e1417b069ea170b34310ab126c6
Josue-JP <josueportillod07@gmail.com>
2025-04-10 11:27:45 -0400

    Update 3.sh

--- scripts/3.sh
+++ scripts/3.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+# Get the current username
+USERNAME=$(whoami)
+
+# Print instructions
 echo "Please edit the line starting with ExecStart according to the name of your user"
-echo "EXAMPLE: "
 echo ""
+echo "EXAMPLE: "
 echo "Before: ExecStart=-/sbin/agetty -o '-- \\u' --noreset --noclear - \${TERM}"
-echo "After: ExecStart=-/sbin/agetty -a \$user --noreset --noclear - \${TERM}"
+echo "After: ExecStart=-/sbin/agetty -a $USERNAME --noreset --noclear - \${TERM}"
+
+# Wait for user to continue
 read -p "Press Any Key to Continue"
+
+# Open the file in Vim for editing
 sudo vim /lib/systemd/system/getty\@.service
-echo "Your System Will Now Reboot To Confirm 'AUTO-LOGIN'" 
+
+# Inform user about reboot
+echo "Your System Will Now Reboot To Confirm 'AUTO-LOGIN'"
+
+# Wait for user to continue before rebooting
 read -p "Press Any Key to Continue"
 sudo reboot
