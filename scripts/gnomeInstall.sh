f558e1bc692d78d12ca3f2a473b36ea68d852506
Josue-JP <josueportillod07@gmail.com>
2025-04-10 22:08:50 -0400

    Update gnomeInstall.sh

--- scripts/gnomeInstall.sh
+++ scripts/gnomeInstall.sh
@@ -13,9 +13,22 @@ USERNAME=$(whoami)
 sudo sed -i "/^\[daemon\]/a AutomaticLoginEnable=True\nAutomaticLogin=$USERNAME" /etc/gdm/custom.conf
 
 echo "Added automatic login for user: $USERNAME"
+if [ -f setup.sh ]; then
+    # setup.sh exists
+    if [ -x setup.sh ]; then
+        echo "setup.sh is executable. Running..."
+        ./setup.sh
+    else
+        echo "setup.sh is not executable. Making it executable..."
+        chmod +x setup.sh
+        echo "Running setup.sh..."
+        ./setup.sh
+    fi
+else
+    echo "setup.sh does not exist."
+fi
+
 echo "Please reboot after entering the Gnome Environment"
 read -p "Press any key to continue"
 sudo systemctl start gdm.service
 
-sleep 5
-reboot
