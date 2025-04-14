848e6b19d5c79f4ded0bad0da5c8ec76c5de3a3b
Josue-JP <josueportillod07@gmail.com>
2025-04-14 15:32:06 -0400

    Update 2.sh

--- scripts/2.sh
+++ scripts/2.sh
@@ -53,19 +53,11 @@ systemctl enable NetworkManager
 
 # Get the current username
 USERNAME=$(whoami)
+ESCAPED_USERNAME=$(printf '%s\n' "$USERNAME" | sed 's/[\/&]/\\&/g')
 
-# Print instructions
-echo "Please edit the line starting with ExecStart according to the name of your user"
-echo ""
-echo "EXAMPLE: "
-echo "Before: ExecStart=-/sbin/agetty -o '-- \\u' --noreset --noclear - \${TERM}"
-echo "After: ExecStart=-/sbin/agetty -a $USERNAME --noreset --noclear - \${TERM}"
+# Enable auto-login into the current user
+sudo sed -i "s|^ExecStart=-/sbin/agetty .*|ExecStart=-/sbin/agetty -a $ESCAPED_USERNAME --noreset --noclear - \${TERM}|" /lib/systemd/system/getty@.service
 
-# Wait for user to continue
-read -p "Press Any Key to Continue"
-
-# Open the file in Vim for editing
-sudo vim /lib/systemd/system/getty\@.service
 
 
 
